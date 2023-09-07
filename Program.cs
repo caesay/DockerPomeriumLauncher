3dfa733200c6fb5ab9e25cab711e44235021b18d
Caelan Sayler <git@caesay.com>
2023-09-07 15:08:07 +0100

    Missed using from previous commit

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using System.Diagnostics;
 using System.Reflection;
 using System.Text;
 using System.Text.Json;
+using System.Text.RegularExpressions;
 using Docker.DotNet;
 using Docker.DotNet.Models;
 using YamlDotNet.Serialization;
